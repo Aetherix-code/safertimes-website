d453181139df7268607248ba09ae3cf0c49a70b0
Jerry VDP <jerry@djint.net>
2025-06-16 17:09:14 +0300

    Fix data

--- server/api/cities.js
+++ server/api/cities.js
@@ -12,14 +12,28 @@ export default defineCachedEventHandler(async (event) => {
         if (cities) {
             query = { 'siren.data': { '$in': cities } };
         }
-        const docs = await collection.find(query, { projection: { 'siren.time': 1, 'siren.date': 1 } }).sort({ 'date': '-1' }).toArray();
+        // Calculate the date 3 months ago from now
+        const threeMonthsAgo = new Date();
+        threeMonthsAgo.setMonth(threeMonthsAgo.getMonth() - 6);
+
+        // Add alertDate filter to the query
+        query['siren.alertDate'] = { $gt: threeMonthsAgo.toISOString() };
+
+        console.log('Query:', query);
+
+        const docs = await collection.find(query, { projection: { 'siren.time': 1, 'siren.date': 1, 'siren.alertDate': 1 } }).sort({ 'date': '-1' }).toArray();
         const entries = docs.map(entry => entry.siren.time);
+        console.log('Docs', docs);
         let firstDate = null;
+        let earliestDate = null;
         if (entries.length) {
             const lastEntry = docs[docs.length - 1];
             firstDate = lastEntry.siren.date.replace(/\./g, '/');
+            earliestDate = docs[0].siren.date.replace(/\./g, '/');
         }
-        return { entries, firstDate };
+        console.log('First date:', firstDate);
+        console.log('Earliest date:', earliestDate);
+        return { entries, firstDate, earliestDate };
     } catch (e) {
         console.error(e);
         throw createError({
@@ -35,5 +49,5 @@ export default defineCachedEventHandler(async (event) => {
             const queryString = JSON.stringify(query);
             const encodedKey = Buffer.from(queryString).toString('base64');
             return encodedKey;
-        },
+        }
     })
